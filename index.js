5662f6e7e3cf2fb1ee94d7b9d633e5e55aad6f62
Rolf Erik Lekang <me@rolflekang.com>
2015-11-03 20:03:17 +0100

    feat: Add project structure and index

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+if (process.argv.length < 4) {
+  console.error("Needs at least 2 arguments");
+  process.exit(1);
+}
+
+console.log(
+  require(process.argv[2])[process.argv[3]]
+    .apply(this, process.argv.slice(4))
+);
